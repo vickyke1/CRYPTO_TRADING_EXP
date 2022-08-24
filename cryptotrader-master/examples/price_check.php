07e3c0ae74e1c718e6d2d92bd0c0d1bf18efb5c0
vicky ke <90403811+vickyke1@users.noreply.github.com>
2022-08-24 14:30:36 -0400

    Delete cryptotrader-master directory

--- cryptotrader-master/examples/price_check.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-include_once(dirname(__FILE__).'/../coinbase-pro.php');
-
-$g = new coinbaseExchange(CB_KEY,CB_SECRET,CB_PASSPHRASE);
-$g->updatePrices('BTC-EUR');
-$g->printPrices('BTC-EUR');
